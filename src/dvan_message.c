4043049cf515918e9f64b35607452662d74e3477
Daniel Nelson <daniel@sigpwr.com>
2012-03-06 22:35:13 -0800

    Added dvan_message generic constructor

--- src/dvan_message.c
+++ src/dvan_message.c
@@ -1,3 +1,19 @@
+#include <stdlib.h>
 #include "dvan_message.h"
+
+dvan_message_t* dvan_message_create(){
+    dvan_message_t* x;
+    x = malloc(sizeof(dvan_message_t));
+    if (!x) return NULL;
+
+    x->type = 0;
+    x->length = 0;
+    x->data = NULL;
+
+    x->expiry = 0;
+    x->src = NULL;
+    list_init(&x->peers);
+    return x;
+}
 
 
--- src/dvan_message.h
+++ src/dvan_message.h
@@ -1,7 +1,10 @@
 #ifndef _DVAN_MESSAGE_H
 #define _DVAN_MESSAGE_H
 #include <stdint.h>
+#include <time.h>
+#include "list.h"
 #include "dvan_buffer.h"
+#include "dvan_server_client.h"
 
 struct dvan_message_header {
     uint8_t version;
@@ -15,12 +18,18 @@ typedef struct dvan_message {
     uint64_t length; 
     void* data;
 
+    time_t expiry;
+    dvan_server_client_t* src;
+    list_t peers;
 } dvan_message_t;
 
+dvan_message_t* dvan_message_create();
 dvan_message_t* dvan_message_from_buffer(dvan_buffer_t* b);
 dvan_message_t* dvan_message_from_string(char* s);
 
 int dvan_message_to_buffer(dvan_message_t* m, dvan_buffer_t* b);
 int dvan_message_to_string(dvan_message_t* m, dvan_buffer_t* b);
+
+int dvan_message_destroy(dvan_message_t* x);
 
 #endif
