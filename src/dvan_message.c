7dc363eac8cc2f69cab7ff3d27f268f14625320d
Daniel Nelson <daniel@sigpwr.com>
2012-03-06 22:44:37 -0800

    Added constructors, destructor for dvan_message

--- src/dvan_message.c
+++ src/dvan_message.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "dvan_message.h"
 
 dvan_message_t* dvan_message_create(){
@@ -13,7 +14,36 @@ dvan_message_t* dvan_message_create(){
     x->expiry = 0;
     x->src = NULL;
     list_init(&x->peers);
+
     return x;
 }
 
+dvan_message_t* dvan_message_create_from_string(char* s){
+    dvan_message_t* x;
+    if (!s) return NULL;
 
+    x = dvan_message_create();
+    if (!x) return NULL;
+
+    x->length = strlen(s) + 1;
+    x->data = malloc(x->length);
+    if (!x->data){
+        dvan_message_destroy(x);
+        return NULL;
+    }
+
+    strncpy(x->data, s, x->length);
+    x->type = DVAN_MESSAGE_STRING;
+    return x;
+}
+
+int dvan_message_destroy(dvan_message_t* x){
+    if (!x) return -EINVAL;
+
+    list_delete(&x->peers);
+
+    if (x->data) free(x->data);
+    free(x);
+
+    return 0;
+}
--- src/dvan_message.h
+++ src/dvan_message.h
@@ -6,6 +6,12 @@
 #include "dvan_buffer.h"
 #include "dvan_server_client.h"
 
+enum {
+    DVAN_MESSAGE_STRING,
+    DVAN_MESSAGE_CALL,
+    DVAN_MESSAGE_NOTIFICATION
+};
+
 struct dvan_message_header {
     uint8_t version;
     uint8_t type;
@@ -14,10 +20,12 @@ struct dvan_message_header {
 } __attribute__((packed));
 
 typedef struct dvan_message {
+    //Set on creation
     uint8_t type;
     uint64_t length; 
     void* data;
 
+    //Set by server
     time_t expiry;
     dvan_server_client_t* src;
     list_t peers;
@@ -28,7 +36,7 @@ dvan_message_t* dvan_message_from_buffer(dvan_buffer_t* b);
 dvan_message_t* dvan_message_from_string(char* s);
 
 int dvan_message_to_buffer(dvan_message_t* m, dvan_buffer_t* b);
-int dvan_message_to_string(dvan_message_t* m, dvan_buffer_t* b);
+int dvan_message_to_string(dvan_message_t* m, char* s, uint64_t len);
 
 int dvan_message_destroy(dvan_message_t* x);
 
